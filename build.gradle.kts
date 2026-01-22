cce5f9d426149b0672da370182bbd393ae184e24
Miroslav Hýbler <miroslav@peko-studio.cz>
2026-01-22 18:44:46 +0100

    kotlin & ksp version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     `kotlin-dsl`
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.3.0" apply false
+    id("com.google.devtools.ksp") version "2.3.4" apply false
     id("org.jetbrains.dokka") version "1.8.20" apply false
 }
 
