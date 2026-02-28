ff5c14ee306ff40119c682e33f3dc3627aa9de18
Miroslav Hýbler <miroslav@peko-studio.cz>
2026-02-28 20:53:20 +0100

    v2.0.0-alpha03 - Compatibility release

--- mockup-annotations/build.gradle.kts
+++ mockup-annotations/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.miroslavhybler.mockup-annotations"
-version = "2.0.0-alpha02"
+version = "2.0.0-alpha03"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
